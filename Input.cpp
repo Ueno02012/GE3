7e33fa92fce0fbf21a1eacf05d781cc627a73211
Ueno02012 <k023g0064@g.neec.ac.jp>
2024-10-02 12:00:25 +0900

    test

---
+++ Input.cpp
@@ -0,0 +1,12 @@
+#include "Input.h"
+
+void Input::Initialize()
+{
+  // DirectInputのインスタンス生成
+  //Comptr<IDirectInput8>
+}
+
+void Input::Update()
+{
+
+}
---
+++ Input.h
@@ -0,0 +1,22 @@
+#pragma once
+#include<d3d12.h>
+#include<dxgi1_6.h>
+
+class Input
+{
+public: // メンバ変数
+
+  /// <summary>
+  /// 初期化
+  /// </summary>
+  void Initialize();
+
+  /// <summary>
+  /// 更新
+  /// </summary>
+  void Update();
+
+
+
+};
+
